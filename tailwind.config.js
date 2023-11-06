ad43a0e62cab9069c9344ba671a5ec695b782124
libbyou <lcyou95@gmail.com>
2023-11-06 14:13:02 -0600

    added handlebars and js for reviews, installed tailwind elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{html,js}"],
+  content: ["./views/**/*.{html,js}",
+  "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
 }
 
