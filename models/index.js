fd0f1dfa4b71e44c9b7730054c2443547978235f
Estevan <egonz94@gmail.com>
2023-11-06 13:38:57 -0600

    Reworked index.js model relationships

--- models/index.js
+++ models/index.js
@@ -4,29 +4,42 @@ const Review = require('./Review');
 const User = require('./User');
 const ReviewComment = require('./ReviewComment')
 
+
+//--------------------------------
 User.hasMany(OwnedGame, {
     foreignKey: 'username'
 });
-
-User.hasMany(Review, {
+OwnedGame.belongsto(User, {
     foreignKey: 'username'
 });
-
+//--------------------------------
+User.hasMany(Review, {
+    foreignKey: 'review_id'
+});
+Review.belongsTo(User, {
+    foreignKey: 'review_id'
+});
+//--------------------------------
 OwnedGame.hasOne(Game, {
     foreignKey: 'game_id'
 });
-
-Review.hasOne(User, {
-    foreignKey: 'username'
-});
-
-Review.hasOne(Game, {
+Game.belongsTo(OwnedGame, {
     foreignKey: 'game_id'
 });
-
-ReviewComment.belongsTo(Review, {
+//--------------------------------
+Game.hasMany(Review, {
+    foreignKey: 'review_id',
+});
+Review.belongsTo(Game, {
     foreignKey: 'review_id'
-})
+});
+//--------------------------------
+Review.hasMany(ReviewComment, {
+    foreignKey: 'comment_id'
+});
+ReviewComment.belongsTo(Review, {
+    foreignKey: 'comment_id'
+});
 
 
 module.exports = {Game, OwnedGame, Review, ReviewComment, User}
