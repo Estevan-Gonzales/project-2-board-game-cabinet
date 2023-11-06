b7d5dfa6f38dd5d98d9b2712e2866003509db386
Estevan <egonz94@gmail.com>
2023-11-06 12:51:10 -0600

    Added ReviewComment model. Edited index.js relationships

--- models/index.js
+++ models/index.js
@@ -2,7 +2,31 @@ const Game = require('./Game');
 const OwnedGame = require('./OwnedGame');
 const Review = require('./Review');
 const User = require('./User');
+const ReviewComment = require('./ReviewComment')
+
+User.hasMany(OwnedGame, {
+    foreignKey: 'username'
+});
+
+User.hasMany(Review, {
+    foreignKey: 'username'
+});
+
+OwnedGame.hasOne(Game, {
+    foreignKey: 'game_id'
+});
+
+Review.hasOne(User, {
+    foreignKey: 'username'
+});
+
+Review.hasOne(Game, {
+    foreignKey: 'game_id'
+});
+
+ReviewComment.belongsTo(Review, {
+    foreignKey: 'review_id'
+})
 
 
-
-module.exports = {Game, OwnedGame, Review, User}
+module.exports = {Game, OwnedGame, Review, ReviewComment, User}
